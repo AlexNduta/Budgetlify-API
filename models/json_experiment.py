0dc1109c89cf4000f7901cd27e777fdf7ae99d8a
Alex Nduta <wasremoved@gmail.com>
2024-02-13 09:58:54 +0300

    I can now successfully save ad retrieve JSOn objects from a file

--- models/json_experiment.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import json
-
-filename = 'test.txt'
-with open(filename, 'r', encoding = 'utf-8') as f:
-    my_data = json.loads(f.read())
-    
-print(my_data['people'][0]['name'])
--- models/reader.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-filename = 'storage.txt'
-
-with open(filename, 'r') as f:
-    file_content = f.read()
-    for item in file_content:
-        print(item)
